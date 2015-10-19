091a4bb235147a3c47debd4c213239d63848d646
fengkai <fkdhy@163.com>
2015-10-19 20:15:58 +0800

    c

---
+++ pyml/jobs/inject_workexperience.py
@@ -0,0 +1,34 @@
+import json
+import re
+import MySQLdb
+
+try:
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', use_unicode=True, charset='utf8')
+    cur = conn.cursor()
+    cur.execute('set character_set_client=utf8')
+    cur.execute('set character_set_connection=utf8')
+    cur.execute('set character_set_database=utf8')
+    cur.execute('set character_set_results=utf8')
+    cur.execute('set character_set_server=utf8')
+    conn.commit()
+    with open('d:/jobs/practice.json') as file:
+        while True:
+            line = file.readline()
+            if line:
+                sc = json.loads(line)
+                uuid = sc['id']
+                workexperiencelst = sc['workExperienceList']
+                for workdct in workexperiencelst:
+                    print workdct
+                    worksql = 'insert into workexperience (userid, department, end_date, industry, position_name, salary, size, start_date, type) values \
+                    ("%s", "%s", "%s", "%s", "%s", %d, %d, "%s", "%s")' % (uuid, workdct['department'], workdct['end_date'], workdct['industry'], workdct['position_name'], workdct['salary'], workdct['size'], workdct['start_date'], workdct['type'])
+                    print worksql
+                    cur.execute(worksql)
+            else:
+                break
+    conn.commit()
+    conn.close()
+except Exception as e:
+    conn.commit()
+    conn.close()
+    print e
