473bf349f36ac058f1903778bbee7b190e987266
fengkai <fkdhy@163.com>
2015-10-19 16:49:29 +0800

    create user and generate result

---
+++ pyml/jobs/generate_result.py
@@ -0,0 +1,34 @@
+#coding:gb2312
+import os
+import json
+import sys
+import re
+import MySQLdb
+reload(sys)
+try:
+    
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', use_unicode=True, charset='utf8')
+    cur = conn.cursor()
+    cur.execute('set character_set_client=utf8')
+    cur.execute('set character_set_connection=utf8')
+    cur.execute('set character_set_database=utf8')
+    cur.execute('set character_set_results=utf8')
+    cur.execute('set character_set_server=utf8')
+    sql = 'select userid from jobs_uinfotest'
+    cur.execute(sql)
+    file = open('d:/jobs/result.csv', 'w+')
+    useridlst = cur.fetchall()
+    file.write('id,degree,size,salary,position_name\n')
+    for userid in useridlst:
+        print userid[0] 
+        strs = userid[0] + u',2,5,6,项目经理\n'
+        file.write(strs.encode('utf-8'))
+        
+    conn.commit()
+    conn.close()
+    file.close()
+except Exception as e:
+    file.close()
+    conn.close()
+    print e
+    
