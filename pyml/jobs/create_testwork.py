b142d89739016260c0dcbfecaa6a9e0a808ce8a7
fengkai <fkdhy@163.com>
2015-10-20 16:06:06 +0800

    add the workexperiencetest

---
+++ pyml/jobs/create_testwork.py
@@ -0,0 +1,16 @@
+import os
+
+import MySQLdb
+try:
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', charset='utf8')
+    cur = conn.cursor()
+    scoresql = 'create table workexperiencetest(id int primary key not null auto_increment, userid varchar(64), \
+    department varchar(255), end_date varchar(12), industry varchar(255), position_name varchar(255),\
+    salary int, size int, start_date varchar(12), type varchar(12))'
+    scoretol = cur.execute(scoresql)
+    conn.close()
+    
+except Exception as e:
+ 
+    conn.close()
+    print e
