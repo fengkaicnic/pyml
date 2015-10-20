1ba98d90b53c3944d8410c5cb150df067a6e6d95
fengkai <fkdhy@163.com>
2015-10-20 16:50:22 +0800

    change table code

--- pyml/jobs/create_user.py
+++ pyml/jobs/create_user.py
@@ -5,7 +5,8 @@ try:
     conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', charset='utf8')
     cur = conn.cursor()
     sql1 = 'drop '
-    scoresql = 'create table jobs_uinfo(id int primary key not null auto_increment, userid varchar(64), age int, degree int, gender varchar(12), major varchar(64))'
+    scoresql = 'create table jobs_uinfo(id int primary key not null auto_increment, userid varchar(64), age int, degree int, gender varchar(12), major varchar(64))\
+    , start_age int, bstart_year int, agenormal float(5, 3), startyearnormal float(5, 3), start_salary int'
     scoretol = cur.execute(scoresql)
     conn.close()
     
--- pyml/jobs/create_workexperience.py
+++ pyml/jobs/create_workexperience.py
@@ -6,7 +6,7 @@ try:
     cur = conn.cursor()
     scoresql = 'create table workexperience(id int primary key not null auto_increment, userid varchar(64), \
     department varchar(255), end_date varchar(12), industry varchar(255), position_name varchar(255),\
-    salary int, size int, start_date varchar(12), type varchar(12))'
+    salary int, size int, start_date varchar(12), type varchar(12)), num int'
     scoretol = cur.execute(scoresql)
     conn.close()
     
