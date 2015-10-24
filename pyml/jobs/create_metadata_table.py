a7510d7082c52125a6656af795d3c5a1eeb7a26f
fengkaicnic <fkdhy@163.com>
2015-10-24 23:17:31 +0800

    change some code

--- pyml/jobs/create_metadata_table.py
+++ pyml/jobs/create_metadata_table.py
@@ -5,7 +5,7 @@ try:
     conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', charset='utf8')
     cur = conn.cursor()
     sql1 = 'drop '
-    metasql = 'create table metadata(id int primary key not null auto_increment, name varchar(255), letter varchar(255), type varchar(20))'
+    metasql = 'create table metadata(id int primary key not null auto_increment, name varchar(128), letter varchar(255), type varchar(20))'
     scoretol = cur.execute(metasql)
     conn.close()
     
