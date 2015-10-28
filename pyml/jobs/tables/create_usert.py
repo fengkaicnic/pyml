05a0f656e045c8ed368af5fe41303906c851f509
fengkaicnic <fkdhy@163.com>
2015-10-28 10:28:33 +0800

    mv table py

---
+++ pyml/jobs/tables/create_usert.py
@@ -0,0 +1,17 @@
+import os
+
+import MySQLdb
+try:
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', charset='utf8')
+    cur = conn.cursor()
+    sql1 = 'drop '
+    scoresql = 'create table jobs_uinfotest(id int primary key not null auto_increment, userid varchar(64), age int, degree int, gender varchar(12), major varchar(128)\
+    , start_age int, bstart_year int, agenormal float(5, 3), startyearnormal float(5, 3), start_salary int)'
+    scoretol = cur.execute(scoresql)
+    conn.close()
+    
+except Exception as e:
+ 
+    conn.close()
+    print e
+    
