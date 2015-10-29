6c8df8c08a877c92ca09817c92b499fe1c131dfd
fengkaicnic <fkdhy@163.com>
2015-10-29 17:15:25 +0800

    add create file

--- pyml/jobs/dctree/generate_train_file.py
+++ pyml/jobs/dctree/generate_train_file.py
@@ -4,7 +4,10 @@ import json
 import sys
 import re
 import MySQLdb
+import time
 reload(sys)
+sys.setdefaultencoding('utf8')
+start = time.clock()
 try:
     
     conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', use_unicode=True, charset='utf8')
@@ -14,15 +17,17 @@ try:
     cur.execute('set character_set_database=utf8')
     cur.execute('set character_set_results=utf8')
     cur.execute('set character_set_server=utf8')
-    sql = 'select userid from jobs_uinfotest'
+    sql = 'select jb.degree,jb.age,jb.start_age,jb.bstart_year,jb.gender,jb.start_salary,wk.size \
+                                        from jobs_uinfo as jb left join workexperience as wk on \
+                                        jb.userid = wk.userid and wk.num = 1'
     cur.execute(sql)
-    file = open('d:/jobs/result.csv', 'w+')
+    file = open('d:/jobs/dctree/dct-train.csv', 'w+')
     useridlst = cur.fetchall()
-    file.write('id,degree,size,salary,position_name\n')
+    file.write('degree,age,start_age,bstart_year,gender,start_salary,start_size\n')
     for userid in useridlst:
-        print userid[0] 
-        strs = userid[0] + u',1,4,1,销售经理\n'
-        file.write(strs.encode('utf-8'))
+        userlst = map(str, userid)
+        strs = ','.join(userlst) + '\n'
+        file.write(strs)
         
     conn.commit()
     conn.close()
@@ -31,4 +36,5 @@ except Exception as e:
     file.close()
     conn.close()
     print e
-    
+end = time.clock()
+print (end - start)
