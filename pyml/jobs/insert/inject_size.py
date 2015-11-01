6305b73f4c749c6b23c59565cf7b21019326feb8
fengkaicnic <fkdhy@163.com>
2015-11-01 19:34:40 +0800

    add a sizetab

---
+++ pyml/jobs/insert/inject_size.py
@@ -0,0 +1,42 @@
+import json
+import re
+import MySQLdb
+import time
+start = time.clock()
+try:
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='123456', db='jobs', use_unicode=True, charset='utf8')
+    cur = conn.cursor()
+    cur.execute('set character_set_client=utf8')
+    cur.execute('set character_set_connection=utf8')
+    cur.execute('set character_set_database=utf8')
+    cur.execute('set character_set_results=utf8')
+    cur.execute('set character_set_server=utf8')
+    conn.commit()
+    with open('d:/jobs/practice.json') as file:
+        lines = file.readlines()
+        linelst = lines[35622:]
+        for line in linelst:
+            if line:
+                sc = json.loads(line)
+                uuid = sc['id']
+                workexperiencelst = sc['workExperienceList']
+                i = 1
+                for workdct in workexperiencelst[:3]:
+                    if workdct is None:
+                        continue
+                    print workdct
+                    worksql = 'insert into work_size (userid, department, end_date, industry, position_name, salary, size, start_date, type, num) values \
+                    ("%s", "%s", "%s", "%s", "%s", %d, %d, "%s", "%s", %d)' % (uuid, workdct['department'], workdct['end_date'], workdct['industry'], workdct['position_name'], workdct['salary'], workdct['size'], workdct['start_date'], workdct['type'], i)
+                    print worksql
+                    cur.execute(worksql)
+                    i += 1
+            else:
+                break
+    conn.commit()
+    conn.close()
+    end = time.clock()
+    print (end - start)
+except Exception as e:
+    conn.commit()
+    conn.close()
+    print e
