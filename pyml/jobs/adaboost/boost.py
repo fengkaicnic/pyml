e0031badae70f8a991ba3c72302c9e5fab32c1ef
fengkaicnic <fkdhy@163.com>
2015-11-19 19:24:46 +0800

    add boost tst

---
+++ pyml/jobs/adaboost/boost.py
@@ -0,0 +1,132 @@
+from numpy import *
+
+import matplotlib.pyplot as plt
+def loadSimpData():
+    datMat = matrix([[1., 2.1],
+                     [2., 1.1],
+                     [1.3, 1.],
+                     [1. , 1.],
+                     [2. , 1.],
+                     ])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+    return datMat, classLabels
+def loadDataSet(filename):
+    numFeat = len(open(filename).readline().split('\t'))
+    dataMat = []
+    labelMat=[]
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr= []
+        curLine = line.strip('\n').split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    fr.close()
+    return dataMat, labelMat
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+    retArray = ones((shape(dataMatrix)[0],1))
+    if threshIneq == 'lt':
+        retArray[dataMatrix[:,dimen]<threshVal] = -1.0
+    else:
+        retArray[dataMatrix[:,dimen]<threshVal] = -1.0
+    return retArray
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = mat(dataArr)
+    labelMat = mat(classLabels).T
+    m,n=shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    bestClassEst = mat(zeros((m,1)))
+    minError = inf
+    for i in range(n):
+        rangeMin = dataMatrix[:,i].min()
+        rangeMax = dataMatrix[:,i].max()
+        stepSize = (rangeMax-rangeMin)/numSteps
+        for j in range(-1, int(numSteps)+1):
+            for inequal in ['lt','gt']:
+                threshVal = (rangeMin+float(j)*stepSize)
+                predictVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+                errArr = mat(ones((m,1)))
+                errArr[predictVals==labelMat]=0
+                weightedError = D.T*errArr
+                print "split: dim %d, thresh %.2f, thresh inequal: %s, the weighted error: %.3f"%\
+                      (i, threshVal, inequal, weightedError)
+            if weightedError < minError:
+                minError = weightedError
+                bestClassEst = predictVals.copy()
+                bestStump['dim']=i
+                bestStump['thresh']=threshVal
+                bestStump['ineq']=inequal
+    return bestStump, minError, bestClassEst
+def adaBoostTrainDS(dataArr, classLabels, numIt = 40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m,1))/m)
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print "D:", D.T
+        alpha = float(0.5*log((1.0-error)/max(error,1e-16)))
+        bestStump['alpha']=alpha
+        weakClassArr.append(bestStump)
+        print "classEst: ", classEst.T
+        expon = multiply(-1*alpha*mat(classLabels).T, classEst)
+        D = multiply(D, exp(expon))
+        D = D/D.sum()
+        aggClassEst += alpha*classEst
+        print "aggClassEst: ", aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m,1)))
+        errorRate = aggErrors.sum()/m
+        print "total error: ", errorRate
+        if errorRate == 0.0:break
+    return weakClassArr
+def adaClassify(datToClass, classifierArr):
+    dataMatrix = mat(datToClass)
+    m =shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'],\
+                                 classifierArr[i]['thresh'],\
+                                 classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha']*classEst
+        print aggClassEst
+    return sign(aggClassEst)
+def plotROC(predStengths, classLabels):
+    cur = (1.0,1.0)
+    ySum = 0.0
+    numPosClas = sum(array(classLabels)==1.0)
+    yStep = 1/float(numPosClas)
+    xStep = 1/float(len(classLabels)-numPosClas)
+    sortedIndicies = predStengths.argsort()
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index]==1.0:
+            delX = 0
+            delY=0
+        else:
+            delX = xStep
+            delY = 0
+            ySum += cur[1]
+        ax.plot([cur[0],cur[0]-delX],[cur[1],cur[1]-delY],c='b')
+        cur =  (cur[0]-delX, cur[1]-delY)
+    ax.plot([0,1],[1,0],'b--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC curve')
+    ax.axis([0,1,0,1])
+    print "The area under curve is:", ySum*xStep
+    plt.show()
+datMat, classLabels = loadSimpData()
+adaBoostTrainDS(datMat, classLabels, 9)
+
+dataArr, labelArr = loadDataSet(r'horseColicTraining2.txt')
+classifierArray = adaBoostTrainDS(dataArr, labelArr, 10)
+testArr, testLabArr = loadDataSet(r'horseColicTest2.txt')
+prediction = adaClassify(testArr, classifierArray)
+plotROC(prediction.T, testLabArr)
+errArr = mat(ones((67,1)))
+error = errArr[prediction!=mat(testLabArr).T].sum()/shape(dataArr)[0]
+print error
