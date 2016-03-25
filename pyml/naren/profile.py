db26a6db7650aecc42ee11e5530665061576806d
fengkaicnic <fkdhy@163.com>
2016-03-25 18:10:36 +0800

    commit new m

---
+++ pyml/naren/profile.py
@@ -0,0 +1,24 @@
+#coding:utf8
+import sys
+import os
+reload(sys)
+import json
+import pdb
+sys.setdefaultencoding('utf8')
+
+with open("d:/naren/data/read/66510-11561507.txt") as file:
+    content = json.load(file)
+
+
+rst = []
+for key in content.iterkeys():
+    print key
+    # rst.append(key + u':' + content[key].decode('utf8'))
+
+pdb.set_trace()
+rst.append(content['selfappraise'].decode('utf8').encode('gbk'))
+rst.append(eval(content['education_history'])[0]['major'].encode('gbk').decode('gbk'))
+
+with open('d:/naren/data/test.txt', 'wb') as file:
+    file.writelines('\n'.join(rst))
+
