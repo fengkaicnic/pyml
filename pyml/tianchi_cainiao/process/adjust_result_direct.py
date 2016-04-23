19a23052ccba117eca8c52f88b38872dba359e17
fengkaicnic <fkdhy@163.com>
2016-04-23 16:55:58 +0800

    commit tmean tstd

--- pyml/tianchi_cainiao/process/adjust_result_direct.py
+++ pyml/tianchi_cainiao/process/adjust_result_direct.py
@@ -44,10 +44,10 @@ for line in lines:
 
     if per > 1.5:
         if adj == 'less':
-            lst[2] = round(float(lst[2]) * (1 - 0.1))
+            lst[2] = round(float(lst[2]) * (1 - 0.25))
         else:
             # pdb.set_trace()
-            lst[2] = round(float(lst[2]) * (1 + 0.1))
+            lst[2] = round(float(lst[2]) * (1 + 0.25))
     rlines.append(','.join(map(lambda x:str(x), lst)))
 
 with open('d:/tianchi/%s.csv' % (fname+'-direct-adj') ,'wb') as fl:
---
+++ pyml/tianchi_cainiao/process/t_meanstd.py
@@ -0,0 +1,68 @@
+import utils
+
+import os
+import pdb
+from scipy import stats
+import traceback
+import numpy as np
+import time
+
+start = time.time()
+
+def update_config(start_date, end_date, doubles, date_tar, double_mv):
+    try:
+        conn = utils.persist.connection()
+        cur = conn.cursor()
+        sql = 'select item_id, store_code from config'
+        cur.execute(sql)
+        rst = cur.fetchall()
+        nm = 0
+        for rs in rst:
+            item_id = rs[0]
+            store_code = rs[1]
+            if store_code == 'all':
+                p_sql = 'select qty_alipay_njhs, date from item_feature where item_id = %d and date \
+                        > "%s" and date <= "%s" ' % (item_id, start_date, end_date)
+            else:
+                p_sql = 'select qty_alipay_njhs, date from item_store_feature where item_id = %d and \
+                         date > "%s" and date <= "%s" and store_code = "%s"' % (item_id, start_date, end_date, store_code)
+
+            cur.execute(p_sql)
+
+            result = cur.fetchall()
+            testarry = [x[0] for x in result]
+            
+            testary = np.array(testarry)
+            testaryn = (testary - testary.mean())/testary.std()
+            if len(testaryn) > 0:
+                stastic, p_value = stats.kstest(testaryn, 'norm')
+            else:
+                p_value = 2.0
+            # print normal.cdf(test_dt[0])
+            # print p_value, item_id, store_code
+            
+            mssql = 'update config set tmean = %f , tstd = %f, normal = %f where item_id = %d and store_code = "%s"' % (testary.mean(),\
+                                                                                             testary.std(), p_value, item_id, store_code)
+            try:
+                cur.execute(mssql)
+            except:
+                traceback.print_exc()
+            nm += 1
+            print nm
+            
+        conn.commit()
+        conn.close()
+    except:
+        traceback.print_exc()
+        pdb.set_trace()
+        conn.close()
+
+
+if __name__ == '__main__':
+    start_date = '20151212'
+    end_date = '20151227'
+
+    update_config(start_date, end_date, 'doublet', '20151212', 'doublet_mv')
+
+end = time.time()
+print end - start
