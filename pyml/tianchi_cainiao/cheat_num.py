faa073aba6e3dfa0d9b8a0cfa3ce9238ed3e937a
fengkaicnic <fkdhy@163.com>
2016-05-13 16:06:26 +0800

    some new code

--- pyml/tianchi_cainiao/cheat_num.py
+++ pyml/tianchi_cainiao/cheat_num.py
@@ -1,12 +1,24 @@
+#76166
 # num = 42868
 # more = 10.66
 # less = 40.14
 # c_num = 5835
 
-num = 5931
-more = 4.68
-less = 16.04
-c_num = 1823
+# num = 5931
+# more = 4.68
+# less = 16.04
+# c_num = 1823
+
+# 39806
+num = -111493
+more = 74.22
+less = 39.66
+c_num = 2503
+
+# num = -6986
+# more = 37.12
+# less = 19.84
+# c_num = 903
 
 rst = (num + more * c_num)/(more + less)
 
--- pyml/tianchi_cainiao/model/generate_feature.py
+++ pyml/tianchi_cainiao/model/generate_feature.py
@@ -2,18 +2,20 @@ import utils
 import traceback
 import pdb
 import time
+import gbdt_model
 
 start = time.time()
 
 try:
     conn = utils.persist.connection()
     cur = conn.cursor()
-    sql = 'select item_id, store_code from config limit 1'
+    sql = 'select item_id, store_code from config limit 3, 1'
     cur.execute(sql)
 
     rst = cur.fetchall()
     num = 0
-
+    period = 7
+    period_l = 14
     results = []
     for rs in rst:
         item_id = rs[0]
@@ -31,32 +33,43 @@ try:
         for i in range(50):
             end1 = end11 + i
             end_date =end_date1 + i
-            if end1 + 14 > the_end:
+            if end1 + period > the_end:
                 break
+            # pdb.set_trace()
             if store_code == 'all':
                 sql_feature = 'select sum(qty_alipay_njhs), sum(pv_ipv), sum(pv_uv), sum(ss_pv_ipv), \
-                            sum(ss_pv_uv), sum(collect_uv) from item_feature where item_id = %d \
-                          and dateid < %d and dateid >= %d' % (item_id, end1 + 14, end1)
-                sql_label = 'select sum(qty_alipay_njhs) from item_feature where item_id = %d \
-                          and dateid < %d and dateid >= %d' % (item_id, end_date + 14, end_date)
+                            sum(ss_pv_uv), sum(collect_uv), sum(qty_alipay - qty_alipay_njhs), sum(ztc_pv_uv),\
+                             sum(tbk_pv_uv) from item_feature where item_id = %d \
+                          and dateid < %d and dateid >= %d' % (item_id, end1 + period, end1)
+                sql_label = 'select (select sum(qty_alipay_njhs) from item_feature where item_id = %d and dateid <%d and\
+                            dateid >= %d) as nexts, (select sum(qty_alipay_njhs) from item_feature where item_id = %d and \
+                            dateid <%d and dateid >=%d) as passs' % (item_id, end_date + 14, end_date, item_id, end_date+14*2, end_date+14)
             else:
                 sql_feature = 'select sum(qty_alipay_njhs), sum(pv_ipv), sum(pv_uv), sum(ss_pv_ipv), \
-                            sum(ss_pv_uv), sum(collect_uv) from item_store_feature where item_id = %d \
-                          and store_code = %s and dateid < %d and dateid >= %d' % (item_id, store_code, end1 + 14, end1)
-                sql_label = 'select sum(qty_alipay_njhs) from item_store_feature where item_id = %d \
-                          and store_code = %s and dateid < %d and dateid >= %d' % (item_id, store_code,\
-                                                                        end_date + 14, end_date)
+                            sum(ss_pv_uv), sum(collect_uv),  sum(qty_alipay - qty_alipay_njhs), sum(ztc_pv_uv), \
+                            sum(tbk_pv_uv) from item_store_feature where item_id = %d \
+                          and store_code = %s and dateid < %d and dateid >= %d' % (item_id, store_code, end1 + period, end1)
+                sql_label = 'select (select sum(qty_alipay_njhs) from item_store_feature where item_id = %d and\
+                              store_code = %s and dateid <%d and dateid >= %d) as nexts, (select sum(qty_alipay_njhs) \
+                              from item_store_feature where item_id = %d and store_code = %s and dateid <%d and dateid >=%d)\
+                               as passs' % (item_id, store_code, end_date + 14, end_date, item_id,\
+                                            store_code, end_date+14*2, end_date+14)
             cur.execute(sql_feature)
             feature = cur.fetchall()
             # pdb.set_trace()
-            if not feature[0][1] :
-                pdb.set_trace()
-            result = [ x for x in feature[0]]
+            # if not feature[0][1] :
+            #     pdb.set_trace()
+            result = []
+            for x in feature[0]:
+                if not x :
+                    result.append(0)
+                else:
+                    result.append(x)
 
             cur.execute(sql_label)
             label = cur.fetchall()
-            result.append(label[0][0]>feature[0][0] and '1' or '0')
-            results.append(','.join(map(lambda x:str(x), result)))
+            result.append(label[0][0]>label[0][1] and 1 or 0)
+            results.append(result)
 
         print item_id
         print store_code
@@ -65,6 +78,9 @@ try:
 
 except:
     traceback.print_exc()
+
+# gbdt_model.gbdt_model(results)
+gbdt_model.svm_model(results, 10)
 
 end = time.time()
 
