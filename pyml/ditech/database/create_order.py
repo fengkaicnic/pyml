cbb04f97d396649358872ec8f43afcd0e34976be
fengkaicnic <fkdhy@163.com>
2016-05-22 19:23:58 +0800

    change order create

--- pyml/ditech/database/create_order.py
+++ pyml/ditech/database/create_order.py
@@ -7,7 +7,7 @@ try:
     conn = utils.persist.connection()
     cur = conn.cursor()
     companysql = 'create table order_data_test(id int primary key not null auto_increment, order_id varchar(64), driver_id varchar(64), passenger_id varchar(64), start_district_hash varchar(64),\
-                    dest_district_hash varchar(64), price decimal(10, 5), time varchar(36))'
+                    dest_district_hash varchar(64), price decimal(10, 5), time varchar(36), date varchar(25), splice int)'
 
     scoretol = cur.execute(companysql)
     conn.commit()
