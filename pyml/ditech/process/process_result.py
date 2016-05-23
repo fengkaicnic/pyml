12bcd79511485b12c86fc651ca591209bc8159ab
fengkaicnic <fkdhy@163.com>
2016-05-23 19:25:56 +0800

    some splice serie

--- pyml/ditech/process/process_result.py
+++ pyml/ditech/process/process_result.py
@@ -1,17 +1,36 @@
 #statstic num of the result
 
-result_dct = {}
-with open('d:/ditech/result_last.csv', 'r') as file:
-    lines = file.readlines()
+import pdb
 
-    for line in lines:
-        rst = line.split(',')
-        if not result_dct.has_key(int(rst[-1])):
-            result_dct[int(rst[-1])] = 1
-        else:
-            result_dct[int(rst[-1])] += 1
+def statstic_num():
+    result_dct = {}
+    with open('d:/ditech/result_last.csv', 'r') as file:
+        lines = file.readlines()
 
-results = sorted(result_dct.items(), key=lambda x:x[0])
+        for line in lines:
+            rst = line.split(',')
+            if not result_dct.has_key(int(rst[-1])):
+                result_dct[int(rst[-1])] = 1
+            else:
+                result_dct[int(rst[-1])] += 1
 
-for rs in results:
-    print rs[0], rs[1]
+    results = sorted(result_dct.items(), key=lambda x:x[0])
+
+    for rs in results:
+        print rs[0], rs[1]
+
+    return results
+
+def simulate_one(results):
+    allnum = 0
+    # pdb.set_trace()
+    allnum += results[0][1]
+    for rs in results[1:]:
+        allnum += rs[1]/(rs[0]/(0/10.0 + 1))
+        # allnum += rs[1]/rs[0]
+    print (2838 - allnum)/2838.0
+
+if __name__ == '__main__':
+
+    results = statstic_num()
+    simulate_one(results)
---
+++ pyml/ditech/process/process_splice_series.py
@@ -0,0 +1,56 @@
+import utils
+import traceback
+import copy
+import time
+
+start = time.time()
+
+path = 'D:/ditech/citydata/read_me_1.txt'
+
+try:
+    splice_lst = []
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            lst = line.split('-')
+            splice_lst.append(int(lst[-1]))
+
+    results = []
+    conn = utils.persist.connection()
+    cur = conn.cursor()
+    sql_hash = 'select * from cluster_map'
+    cur.execute(sql_hash)
+    rst = cur.fetchall()
+    hash_dct = {}
+    for rs in rst:
+        hash_dct[rs[0]] = [rs[0], rs[1]]
+    num = 0
+    for key in hash_dct.keys():
+        rsult = copy.deepcopy(hash_dct[key])
+        num += 1; print num
+        for splice in splice_lst:
+            # sqltf = 'select count(*) from order_data where start_district_hash = "%s" and\
+            #       splice = %d' % (key, splice)
+            sqlntf = 'select date, count(*) from order_data where start_district_hash = "%s" and\
+                  splice = %d and driver_id = "NULL" group by date order by date' % (key, splice)
+
+            # cur.execute(sqltf)
+            # rst1 = cur.fetchall()
+            rsult.append(splice)
+            cur.execute(sqlntf)
+            rst2 = cur.fetchall()
+            for rs in rst2:
+                rsult.append(rs[1])
+        results.append(','.join(map(lambda x:str(x), rsult)))
+
+    with open('d:/ditech/splice_series.csv', 'wb') as file:
+        file.writelines('\n'.join(results))
+
+    conn.close()
+except:
+
+    traceback.print_exc()
+    conn.close()
+
+end = time.time()
+print end - start
