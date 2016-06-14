4fddb3b79560e31f9cb5f97bdacc2ea6cda3f855
fengkaicnic <fkdhy@163.com>
2016-06-14 14:32:52 +0800

    hash gap utils

---
+++ pyml/ditech/optimize/utils.py
@@ -0,0 +1,13 @@
+#coding:utf8
+import sys
+from DBUtils import PersistentDB
+reload(sys)
+import MySQLdb
+
+sys.setdefaultencoding='utf8'
+
+
+persist = PersistentDB.PersistentDB(MySQLdb, host='127.0.0.1', port=3306, user='root',\
+                                    passwd='123456', db='ditech', charset='utf8')
+
+
