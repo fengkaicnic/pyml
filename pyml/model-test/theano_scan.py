669cf372434e8d5c3e420ffe66d061f6540ba120
fengkaicnic <fkdhy@163.com>
2016-06-21 18:02:32 +0800

    theano scan rn

---
+++ pyml/model-test/theano_scan.py
@@ -0,0 +1,22 @@
+import theano
+import theano.tensor as T
+import numpy as np
+
+# defining the tensor variables
+X = T.matrix("X")
+W = T.matrix("W")
+b_sym = T.vector("b_sym")
+
+results, updates = theano.scan(lambda v: T.tanh(T.dot(v, W) + b_sym), sequences=X)
+compute_elementwise = theano.function(inputs=[X, W, b_sym], outputs=results)
+
+# test values
+x = np.eye(2, dtype=theano.config.floatX)
+w = np.ones((2, 2), dtype=theano.config.floatX)
+b = np.ones((2), dtype=theano.config.floatX)
+b[1] = 2
+
+print(compute_elementwise(x, w, b))
+
+# comparison with numpy
+print(np.tanh(x.dot(w) + b))
